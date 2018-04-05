3fbcdedb4bf95df54a168770e1d24d8c522925ba
mrbmadrid <mrbmadrid@gmail.com>
2018-04-05 13:58:00 -0700

    Added form submition

--- server.py
+++ server.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
+
 @app.route('/')
 def index():
 	return render_template('index.html')
+
 @app.route('/ninja')
 def ninjas():
 	return render_template('ninja.html')
-@app.route('/dojos/new')
+
+@app.route('/form')
 def new():
 	return render_template('form.html')
+
+@app.route('/process', methods=['POST', 'GET'])
+def form_process():
+	print request.form['name']
+	return redirect('/')
 app.run(debug=True)
 	
 	 
